ff670624aa3e20361983bbb33999cd54d043c7c2
842974287 <842974287@qq.com>
2017-06-21 11:10:20 +0800

    models

--- LibrarySystem/Library/models.py
+++ LibrarySystem/Library/models.py
@@ -1,20 +1,44 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-
+from django.utils import timezone
+from datetime import timedelta
 # Create your models here.
 
-class List(models.Model):
-    Id = models.IntegerField(primary_key = True, verbose_name = u'ID')
-    Name = models.CharField(max_length = 20, verbose_name = u'姓名')
-    Telephone = models.CharField(max_length = 15, verbose_name = u'手机号码')
-    TimeOne = models.CharField(max_length = 300, verbose_name = u'6月24日 周六10:10-11:40')
-    TimeTwo = models.CharField(max_length = 300, verbose_name = u'6月27日 周二早上3-4节')
-    TimeThree = models.CharField(max_length = 300, verbose_name = u'7月2日 周日14:00-17:00')
-    TimeFour = models.CharField(max_length = 300, verbose_name = u'7月4日 周二9:00-11:00')
-    TimeFive = models.CharField(max_length = 300, verbose_name = u'7月8日 周六10:10-11:40')
-    TimeSix = models.CharField(max_length = 300, verbose_name = u'7月9日 周日08:30-10：00')
-    TimeSeven = models.CharField(max_length = 300, verbose_name = u'7月11日 周二早上3-4节')
-    TimeEight = models.CharField(max_length = 300, verbose_name = u'7月16日 周日08:30-10:00')
+class Book(models.Model):
+    ISBN = models.CharField(max_length = 20, primary_key = True, verbose_name = u'ISBN')
+    BookName = models.CharField(max_length = 100, verbose_name = u'书名')
+    PublishingCompany = models.CharField(max_length = 100, verbose_name = u'出版社')
+    Author = models.CharField(max_length = 100, verbose_name = u'作者')
+    Storage = models.IntegerField(u'馆藏数量')
+    Number = models.IntegerField(u'可借数量')
+    IsAvailuable = models.BooleanField(u'是否可借')
 
     def __unicode__(self):
-        return str(self.Id) + '_' + self.Name
+        return self.BookName + '_' + self.Author
+
+class Reader(models.Model):
+    GENDER_CHOICES = (
+        (0, 'Unknown'),
+        (1, 'Male'),
+        (2, 'Female'),
+    )
+    ReaderId = models.CharField(max_length = 20, primary_key = True, verbose_name = u'读者Id')
+    ReaderName = models.CharField(max_length = 20, verbose_name = u'姓名')
+    Gender = models.IntegerField(default = 0, choices = GENDER_CHOICES, verbose_name = u'性别')
+    MaxBook = models.IntegerField(default = 10, verbose_name = u'可借数量')
+    Borrowed = models.IntegerField(default = 0, verbose_name = u'已借数量')
+    Telephone = models.CharField(max_length = 15, verbose_name = u'联系电话')
+
+    def __unicode__(self):
+        return self.ReaderId + '_' + self.ReaderName
+
+class BorrowList(models.Model):
+    ListId = models.CharField(max_length = 50, primary_key = True, verbose_name = u'ID')
+    Reader = models.ForeignKey(Reader)
+    Book = models.ForeignKey(Book)
+    BorrowDate = models.DateTimeField(auto_now_add = True, verbose_name = u'借出日期')
+    DeadLine = models.DateTimeField(default = timezone.now() + timedelta(days = 20), verbose_name = u'归还日期')
+    Panelty = models.IntegerField(default = 0, verbose_name = u'罚款')
+
+    def __unicode__(self):
+        return self.ListId
